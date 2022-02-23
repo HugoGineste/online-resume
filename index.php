e30b04e83cae640706633a4f55126987f179aa9e
HugoGineste <74255450+HugoGineste@users.noreply.github.com>
2022-02-23 17:36:45 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html> 
+<html> 
+   <head> 
+      <meta charset="UTF-8" /> 
+      <style> 
+      *{ 
+         /* Code CSS */ 
+         font-family:verdana; 
+         font-size:10pt; 
+      } 
+      </style> 
+   </head> 
+   <body> 
+      <div>C'est du HTML.</div> 
+      <script language="javascript"> 
+         document.write("C'est du Javascript."); 
+      </script><br /> 
+      Et là, du PHP!    
+   </body> 
+  </html>
